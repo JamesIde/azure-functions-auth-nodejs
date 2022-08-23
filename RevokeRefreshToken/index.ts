a9ee1d10f7560fe3b97faccbc3fedf129907ab4b
James <james.ide775@gmail.com>
2022-08-23 09:44:56 +0930

    Revoke refresh token added

---
+++ RevokeRefreshToken/index.ts
@@ -0,0 +1,32 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { connectDb } from "../utilities/config/connectDb"
+import { IUser, User } from "../utilities/models/userModel"
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  /*
+    We accept a user id as a param
+    Query the database for that user,
+    Update the users token version to increment it by one
+    */
+  try {
+    await connectDb()
+    const userId: string = req.query.id || context.req.query.id
+    // Find user and update token version
+    await User.findOneAndUpdate({ id: userId }, { $inc: { tokenVersion: 1 } })
+
+    context.res = {
+      status: 200,
+      body: true,
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error,
+    }
+  }
+}
+
+export default httpTrigger
